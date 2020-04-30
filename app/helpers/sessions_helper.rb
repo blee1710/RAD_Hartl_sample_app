4c66fb2630c68e78317601dc07fc79be28699abf
blee1710 <s3707616@student.rmit.edu.au>
2020-04-30 21:32:20 +1000

    Tute 8 Production

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,8 +39,24 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns true if user is logged in, false otherwise
   def logged_in?
     !current_user.nil?
   end
+
+  # Redirects to stored location / or default
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # stores url trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  
 end
